40b91a7df8d0414bf7db5bef0d24cf7c225aca7d
Mohamed Dembele <modembfr@gmail.com>
2019-07-02 13:54:32 -0400

    ...

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,28 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+// use App\Models\Passport\Client;
+// use App\Models\Passport\Token;
+// use App\Models\Passport\AuthCode;
+// use App\Models\Passport\PersonalAccessClient;
+
+// use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array
+     */
+    protected $policies = [
+        'App\Model' => 'App\Policies\ModelPolicy',
+    ];
+
     /**
      * Register any application services.
      *
@@ -23,6 +41,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Schema::defaultStringLength(191);
+        $this->registerPolicies();    
+        Passport::routes();
+
+        Passport::enableImplicitGrant();
+    
+        // Passport::tokensExpireIn(now()->addDays(15));    
+        // Passport::refreshTokensExpireIn(now()->addDays(30));    
+        // Passport::personalAccessTokensExpireIn(now()->addMonths(6));
+
+        // Passport::useTokenModel(Token::class);
+        // Passport::useClientModel(Client::class);
+        // Passport::useAuthCodeModel(AuthCode::class);
+        // Passport::usePersonalAccessClientModel(PersonalAccessClient::class);
     }
 }
